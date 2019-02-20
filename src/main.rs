7ab191d8d554a38b752a191015fa434df8816233
ahui2016 <ahui.2015@icloud.com>
2019-02-20 18:16:41 +0800

    尝试通过 Fairing 跳转
    尝试通过 Fairing 跳转到另一个 URI, 即通过 Fairing 实现一个类似 Redirect 的功能,
     成功。

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,11 @@ use std::env;
 use std::sync::Mutex;
 
 use dotenv::dotenv;
+use rocket::fairing::{self, Fairing};
+use rocket::http::uri::Origin;
 use rocket::request::{FlashMessage, Form};
 use rocket::response::{Flash, Redirect};
-use rocket::State;
+use rocket::{Request, State, Data};
 use rocket_contrib::templates::Template;
 
 mod schema;
@@ -26,35 +28,64 @@ mod schema;
 #[database("mimadb")]
 pub struct DbConn(diesel::PgConnection);
 
-struct Password {
+struct Login {
     password: Mutex<&'static str>,
+    // TODO: 增加一个时间戳
+    status: LoginStatus,
+}
+enum LoginStatus {
+    NewAccount,
+    LoggedOut,
+    LoggedIn,
+    Expired,
 }
 
 fn main() {
     dotenv().ok();
-    let _rocket_databases = env::var("ROCKET_DATABASES")
-        .expect("ROCKET_DATABASES must be set");
+    let _rocket_databases = env::var("ROCKET_DATABASES").expect("ROCKET_DATABASES must be set");
 
-    let password_state = Password {
+    let password_state = Login {
         password: Mutex::new(""),
+        status: LoginStatus::NewAccount,
     };
 
     rocket::ignite()
         .attach(DbConn::fairing())
-        .mount("/", routes![get_password, change_password])
+        .mount("/", routes![index, get_password, change_password])
         .attach(Template::fairing())
         .manage(password_state)
+        .attach(LoginFairing)
         .launch();
 }
 
+#[get("/")]
+fn index(state: State<Login>, conn: DbConn) -> String {
+    "It's index.".into()
+}
+
 #[get("/get_password")]
-fn get_password(state: State<Password>) -> String {
+fn get_password(state: State<Login>) -> String {
     format!("Password is {}", state.password.lock().unwrap())
 }
 
 #[get("/change_password")]
-fn change_password(state: State<Password>) -> Redirect {
+fn change_password(state: State<Login>) -> Redirect {
     let mut pwd = state.password.lock().unwrap();
     *pwd = "new password";
     Redirect::to(uri!(get_password))
+}
+
+pub struct LoginFairing;
+impl Fairing for LoginFairing {
+    fn info(&self) -> fairing::Info {
+        fairing::Info {
+            name: "GET/Post Login",
+            kind: fairing::Kind::Request,
+        }
+    }
+    fn on_request(&self, request: &mut Request, _: &Data) {
+        if request.uri().path() != "/get_password" {
+            request.set_uri(Origin::parse("/").unwrap());
+        }
+    }
 }
