41bc31803ddbacf91acf5b658aa22238a897be0e
ahui2016 <ahui.2015@icloud.com>
2019-02-21 14:11:19 +0800

    尝试在 fairing 里访问数据库，成功。
    貌似无法直接使用 request.guard::<T>()， 只好直接用 PgConnection::establish 来连接数据库。

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,41 @@ extern crate serde_derive;
 #[macro_use]
 extern crate diesel;
 
+extern crate chrono;
 extern crate dotenv;
+extern crate uuid;
+// extern crate time;
 
 use std::borrow::BorrowMut;
 use std::env;
 use std::sync::Mutex;
 
+use chrono::{Duration, NaiveDateTime, Utc};
+use diesel::prelude::*;
+use diesel::sql_types::{Binary, Text};
 use dotenv::dotenv;
 use rocket::fairing::{self, Fairing};
 use rocket::http::uri::Origin;
+use rocket::http::Method;
 use rocket::request::{FlashMessage, Form};
 use rocket::response::{Flash, Redirect};
-use rocket::{Request, State, Data};
+use rocket::{Data, Request, State};
 use rocket_contrib::templates::Template;
+use uuid::Uuid;
 
 mod schema;
+
+static PASSWORD: &'static str = "abcabc";
+sql_function!(pgp_sym_encrypt, T_pgp_sym_encrypt, (x: Text, y:Text) -> Binary);
+sql_function!(pgp_sym_decrypt, T_pgp_sym_decrypt, (x: Binary, y:Text) -> Text);
 
 #[database("mimadb")]
 pub struct DbConn(diesel::PgConnection);
 
 struct Login {
     password: Mutex<&'static str>,
-    // TODO: 增加一个时间戳
-    status: LoginStatus,
-}
-enum LoginStatus {
-    NewAccount,
-    LoggedOut,
-    LoggedIn,
-    Expired,
+    datetime: NaiveDateTime,
+    period: Duration, // datetime + period == expired time
 }
 
 fn main() {
@@ -46,21 +52,22 @@ fn main() {
 
     let password_state = Login {
         password: Mutex::new(""),
-        status: LoginStatus::NewAccount,
+        datetime: Utc::now().naive_utc(),
+        period: Duration::minutes(30), // the default life of the password
     };
 
     rocket::ignite()
         .attach(DbConn::fairing())
-        .mount("/", routes![index, get_password, change_password])
+        .mount("/", routes![new_account, get_password, change_password])
         .attach(Template::fairing())
         .manage(password_state)
         .attach(LoginFairing)
         .launch();
 }
 
-#[get("/")]
-fn index(state: State<Login>, conn: DbConn) -> String {
-    "It's index.".into()
+#[get("/new_account")]
+fn new_account(state: State<Login>, conn: DbConn) -> String {
+    "It's a new account.".into()
 }
 
 #[get("/get_password")]
@@ -84,8 +91,36 @@ impl Fairing for LoginFairing {
         }
     }
     fn on_request(&self, request: &mut Request, _: &Data) {
-        if request.uri().path() != "/get_password" {
-            request.set_uri(Origin::parse("/").unwrap());
+        let database_url = env::var("DATABASE_URL").unwrap();
+        let conn = PgConnection::establish(&database_url).unwrap();
+        let result = schema::allmima::table
+            .select(schema::allmima::id)
+            .first::<String>(&conn);
+        if result.unwrap_err() == diesel::NotFound {
+            request.set_method(Method::Get);
+            request.set_uri(Origin::parse("/new_account").unwrap());
+        } else {
+            request.set_uri(Origin::parse("/get_password").unwrap());
         }
+        //         let password = schema::allmima
+        //             .filter(
+        //                 schema::allmima::title
+        //                     .eq("")
+        //                     .and(schema::allmima::username.eq("")),
+        //             )
+        //             .select(pgp_sym_decrypt(schema::allmima::password, PASSWORD))
+        //             .get_result();
     }
+}
+
+#[derive(Serialize, Queryable, Debug, Clone)]
+pub struct Mima {
+    pub id: String,
+    pub title: String,
+    pub username: String,
+    pub passowrd: Option<Vec<u8>>,
+    pub notes: Option<Vec<u8>>,
+    pub favorite: bool,
+    pub created: String,
+    pub deleted: String,
 }
